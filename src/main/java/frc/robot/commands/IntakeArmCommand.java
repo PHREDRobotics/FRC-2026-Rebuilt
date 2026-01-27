a51d37e7f572336888449dddac6468ad7559cf4b
Maddox-2 <phredteam847@gmail.com>
2026-01-26 18:34:27 -0800

    Intake Arm Command finished

--- src/main/java/frc/robot/commands/IntakeArmCommand.java
+++ src/main/java/frc/robot/commands/IntakeArmCommand.java
@@ -1,5 +1,36 @@
 package frc.robot.commands;
 
-public class IntakeArmCommand {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intakeArm.IntakeArmSubsystem;
+public class IntakeArmCommand extends Command {
+
+    private IntakeArmSubsystem m_IntakeArmSubsystem;
     
+    public IntakeArmCommand(IntakeArmSubsystem IntakeArmSubsystem ) {
+      m_IntakeArmSubsystem = IntakeArmSubsystem;
+
+      addRequirements(IntakeArmSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        m_IntakeArmSubsystem.startIntakeArmExtend();
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public boolean isFinished() {
+       return m_IntakeArmSubsystem.isIntakeArmExtended();
+    }
+    
+    @Override
+    public void end(boolean interrupted) {
+      m_IntakeArmSubsystem.stopIntakeArm();
+    }
 }
+
+
+
--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -27,7 +27,6 @@ public class ShooterCommand extends Command {
 
   @Override
   public void execute() {
-
   }
 
   @Override
