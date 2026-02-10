18e60206c3d7f95da7c2b2bb65c8fa7312c33048
Phred <phredteam847@gmail.com>
2026-02-09 20:51:32 -0800

    Worked on Choreo Paths.

--- src/main/java/frc/robot/choreo/ChoreoVars.java
+++ src/main/java/frc/robot/choreo/ChoreoVars.java
@@ -10,8 +10,18 @@ import edu.wpi.first.units.measure.*;
  * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
  * in the Choreo GUI.
  */
-@SuppressWarnings("unused")
 public final class ChoreoVars {
+
+    public static final class Poses {
+        public static final Pose2d StartPositionOne = new Pose2d(3.893, 7.432, Rotation2d.kZero);
+        public static final Pose2d ShootPositionOne = new Pose2d(2.555, 5.305, Rotation2d.fromRadians(-0.549));
+        public static final Pose2d StartPositionTwo = new Pose2d(3.442, 4.033, Rotation2d.kZero);
+        public static final Pose2d ShootPositionTwo = new Pose2d(2.356, 4.033, Rotation2d.kZero);
+        public static final Pose2d StartPositionThree = new Pose2d(3.431, 0.659, Rotation2d.kZero);
+        public static final Pose2d ShootPositionThree = new Pose2d(2.64, 2.586, Rotation2d.fromRadians(0.658));
+
+        private Poses() {}
+    }
 
     private ChoreoVars() {}
 }
